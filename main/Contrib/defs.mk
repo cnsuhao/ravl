e730559dacb526533d8672d0b728dc830dd547d9
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2003-02-19 12:10:07 +0000

    Mention DV.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@2414 7226f50c-d813-0410-99bf-979b288ab488

--- main/Contrib/defs.mk
+++ main/Contrib/defs.mk
@@ -7,6 +7,6 @@
 #! rcsid="$Id$"
 #! file="Contrib/defs.mk"
 
-NESTED=V4L.r Meteor1.r HTTP. ClipStationPro DMedia MPEG
+NESTED=V4L.r Meteor1.r HTTP ClipStationPro DMedia MPEG DV
 
 EHT = Ravl.Contrib.html Ravl.Contrib.Image_IO.html
