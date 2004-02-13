ac95e709a9b75d140a40c6d057b0533a8cf5c8ea
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2004-02-13 13:17:37 +0000

    FFT wrappers for intel library.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@3933 7226f50c-d813-0410-99bf-979b288ab488

---
+++ main/Contrib/IntelMKL/defs.mk
@@ -0,0 +1,23 @@
+# This file is part of RAVL, Recognition And Vision Library 
+# Copyright (C) 2004, University of Surrey
+# This code may be redistributed under the terms of the GNU Lesser
+# General Public License (LGPL). See the lgpl.licence file for details or
+# see http://www.gnu.org/copyleft/lesser.html
+# file-header-ends-here
+#! rcsid="$Id$"
+
+PACKAGE=Ravl
+
+HEADERS=IntelFFT2d.hh
+
+SOURCES=IntelFFT2d.cc
+
+TESTEXE= testIntelFFT2d.cc
+
+PLIB=RavlIntelMKL
+
+USESLIBS=IntelMKL RavlMath
+
+AUXFILES=IntelMKL.def
+
+AUXDIR=lib/RAVL/libdep
