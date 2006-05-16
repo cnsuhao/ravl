824a4f5b527dad068802c2dc3a40171c29056cdc
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2006-05-16 08:57:43 +0000

    Remove old tags.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@5477 7226f50c-d813-0410-99bf-979b288ab488

--- rel/start/RavlDoc/www/search/Src/DataBaseSettings.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$DBHost     = "mysql.sourceforge.net";		//MySQL-Server
-$DBName     = "ravl";		//MySQL-Database
-$DBUser     = "ravl";		//MySQL-Username
-$DBPassword = "h7xiylns";		//MySQL-Password
-
-
-?>
