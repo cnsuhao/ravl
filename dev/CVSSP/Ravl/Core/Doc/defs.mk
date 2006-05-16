9fc4c5f56b69f883567eb175dfcba97cff7648a8
robowaz <robowaz@7226f50c-d813-0410-99bf-979b288ab488>
2006-05-16 10:05:44 +0000

    Moved release tag to correct location. Removed old branch.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@5481 7226f50c-d813-0410-99bf-979b288ab488

--- dev/CVSSP/Ravl/Core/Doc/defs.mk
+++
@@ -1,6 +0,0 @@
-
-EHT= Ravl.Introduction.Debugging.html \
- Ravl.Introduction.Reference_Counting.html Ravl.Introduction.Naming_Conventions.html \
- Ravl.Introduction.Reference_Counting.Notes.html
-
-HTML = search.html
