80db64472214224b12e658aa002e94cb4fe03f60
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2006-06-16 15:52:34 +0000

    Use no copy version if available.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@5599 7226f50c-d813-0410-99bf-979b288ab488

--- main/Contrib/HTTPServer/HTTPResponse.cc
+++ main/Contrib/HTTPServer/HTTPResponse.cc
@@ -39,23 +39,46 @@ namespace RavlN
   {
   }
   
+
+#ifdef EHS_HAVE_SETBODYNOCOPY
+  static void DestoryStringBuffer(char *data,void *userData) {
+    StringC *buff = static_cast<StringC *>(userData);
+    delete buff;
+  }
+#endif
   
   
   bool HTTPResponseC::SetText(StringC &text)
   {
     RavlAssertMsg(m_response, "HTTPResponseC::SetText no valid response");
     
-    m_response->SetBody(text.chars(), text.length());
+#ifdef EHS_HAVE_SETBODYNOCOPY
+    StringC *handle = new StringC(text);
+    m_response->SetBodyNoCopy(const_cast<char *>(text.chars()), text.length(),DestoryStringBuffer,handle);
+#else
+    m_response->SetBody(text.chars(), text.length());    
+#endif
     
     return true;
   }
   
+#ifdef EHS_HAVE_SETBODYNOCOPY
+  static void DestorySArrayBuffer(char *data,void *userData) {
+    SArray1dC<char> *buff = static_cast<SArray1dC<char> *>(userData);
+    delete buff;
+  }
+#endif
   
   bool HTTPResponseC::SetData(SArray1dC<char> &data)
   {
     RavlAssertMsg(m_response, "HTTPResponseC::SetData no valid response");
     
+#ifdef EHS_HAVE_SETBODYNOCOPY
+    SArray1dC<char> *handle = new SArray1dC<char>(data);
+    m_response->SetBodyNoCopy(data.DataStart(), data.Size(),DestorySArrayBuffer,handle);
+#else
     m_response->SetBody(data.DataStart(), data.Size());
+#endif
     
     return true;
   }
