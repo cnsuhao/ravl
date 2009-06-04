bacf6246eab90115caebd938b5c032a1271ddd9a
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2009-06-04 16:24:23 +0000

    Ran 'annotate' on dir
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@7197 7226f50c-d813-0410-99bf-979b288ab488

--- main/Ravl/Contrib/Plotting/GnuPlot/defs.mk
+++ main/Ravl/Contrib/Plotting/GnuPlot/defs.mk
@@ -1,3 +1,9 @@
+# This file is part of RAVL, Recognition And Vision Library 
+# Copyright (C) 2006, University of Surrey
+# This code may be redistributed under the terms of the GNU Lesser
+# General Public License (LGPL). See the lgpl.licence file for details or
+# see http://www.gnu.org/copyleft/lesser.html
+# file-header-ends-here
 
 DONOT_SUPPORT=VCPP
 
@@ -6,8 +12,6 @@ PACKAGE=Ravl/Plot
 HEADERS = GnuPlot.hh
 
 REQUIRES = GnuPlot
-
-PACKAGE=Ravl/Plot
 
 SOURCES = GnuPlot.cc
 
