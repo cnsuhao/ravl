1867cc1aba40c52bad862e256c32cea07b57de9b
craftit <craftit@7226f50c-d813-0410-99bf-979b288ab488>
2010-02-21 09:46:39 +0000

    Use SizeT for object id's.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@7577 7226f50c-d813-0410-99bf-979b288ab488

--- main/Ravl/Core/System/PointerManager.cc
+++ main/Ravl/Core/System/PointerManager.cc
@@ -47,7 +47,7 @@ namespace RavlN {
     if(!strm.PointerManager().IsValid())
       strm.PointerManager() = PointerManagerC(true);
     PointerManagerC mgr(strm.PointerManager());
-    UIntT id;
+    SizeT id;
     if(mgr.Lookup(obj,id)) { // Seen this object already ?
       ONDEBUG(cerr << "Storing id " << id << " from " << *((void **)obj.Pointer()) << "\n");
       strm << id;
@@ -67,7 +67,7 @@ namespace RavlN {
     if(!strm.PointerManager().IsValid())
       strm.PointerManager() = PointerManagerC(false);
     PointerManagerC mgr(strm.PointerManager());
-    UIntT id;
+    SizeT id;
     strm >> id;
     StoredPointerC *ptr = mgr.Lookup(id);
     if(ptr != 0) { // Seen this object already ?
--- main/Ravl/Core/System/PointerManager.hh
+++ main/Ravl/Core/System/PointerManager.hh
@@ -193,25 +193,25 @@ namespace RavlN {
     ~PointerManagerBodyC();
     //: Destructor.
     
-    bool Lookup(IOPtrC &obj,UIntT &id) 
+    bool Lookup(IOPtrC &obj,SizeT &id)
     { return ptr2id.Lookup(obj.Key(),id); }
     //: Test if an object has already been saved/loaded.
     // If it has then Lookup will return true and id will
     // be set to the id of the object.
     
-    UIntT Insert(IOPtrC &obj) {
-      UIntT id = idAlloc++;
+    SizeT Insert(IOPtrC &obj) {
+      SizeT id = idAlloc++;
       ptr2id[obj.Key()] = id;
       return id;
     }
     //: Store the address of an object.
     // returns a unique id for that object.
     
-    StoredPointerC *Lookup(UIntT id) 
+    StoredPointerC *Lookup(SizeT id)
     { return id2ptr.Lookup(id); }
     //: Lookup object by id.
     
-    void Insert(UIntT id,IOPtrC &obj) { 
+    void Insert(SizeT id,IOPtrC &obj) {
       //RavlAssert(id == idAlloc); // Check sequence.
       //idAlloc++;
       obj.Actions()->Store(id2ptr[id],obj);
@@ -222,9 +222,9 @@ namespace RavlN {
     { return idAlloc; }
     //: Access current alloc id.
   protected:
-    UIntT idAlloc;
-    HashC<UIntT,StoredPointerC> id2ptr;
-    HashC<void *,UIntT> ptr2id;
+    SizeT idAlloc;
+    HashC<SizeT,StoredPointerC> id2ptr;
+    HashC<void *,SizeT> ptr2id;
   };
   
   //! userlevel=Advanced
@@ -269,26 +269,26 @@ namespace RavlN {
     //: Body access.
     
   public:
-    bool Lookup(IOPtrC &obj,UIntT &id)
+    bool Lookup(IOPtrC &obj,SizeT &id)
     { return Body().Lookup(obj,id); }
     //: Test if an object has already been saved/loaded.
     // If it has then Lookup will return true and id will
     // be set to the id of the object.
     
-    StoredPointerC *Lookup(UIntT id) 
+    StoredPointerC *Lookup(SizeT id)
     { return Body().Lookup(id); }
     //: Lookup object by id.
     
-    UIntT Insert(IOPtrC &obj)
+    SizeT Insert(IOPtrC &obj)
     { return Body().Insert(obj); }
     //: Store the address of an object.
     // returns a unique id for that object.
     
-    void Insert(UIntT id,IOPtrC &obj)
+    void Insert(SizeT id,IOPtrC &obj)
     { Body().Insert(id,obj); }
     //: Store by id.
     
-    UIntT IdAlloc() const
+    SizeT IdAlloc() const
     { return Body().IdAlloc(); }
     //: Access current alloc id.
   };
