fb16e35a44f7f25b1031be9fc7b202cdc9ddaaa6
alexkostin <alexkostin@7226f50c-d813-0410-99bf-979b288ab488>
2010-02-25 14:39:40 +0000

    Spelling
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@7597 7226f50c-d813-0410-99bf-979b288ab488

--- main/Ravl/Core/System/VirtualConstructor.hh
+++ main/Ravl/Core/System/VirtualConstructor.hh
@@ -125,7 +125,7 @@ namespace RavlN {
   
   bool CreateVirtualConstructorAlias(const char *newName,const char *oldName);
   //: Create alias for type.
-  //: This can be used to allow classes that have been renamed to be loaded for backward compatibilty.
+  //: This can be used to allow classes that have been renamed to be loaded for backward compatibility.
   
 #define RAVL_INITVIRTUALCONSTRUCTOR(bodyname) \
   static RavlN::VirtualConstructorInstC<bodyname> vc_function_ ## bodyname(# bodyname );
@@ -133,7 +133,7 @@ namespace RavlN {
 #define RAVL_INITVIRTUALCONSTRUCTOR_NAMED(bodyname,name) \
   static RavlN::VirtualConstructorInstC<bodyname> vc_function_ ## bodyname(name );
 
-  // Just initalise the virtual constructor.
+  // Just initialise the virtual constructor.
 
 #define RAVL_VIRTUALCONSTRUCTOR_HANDLE(bodyname,handlename,basename) \
  handlename::handlename(std::istream &strm) \
@@ -149,7 +149,7 @@ namespace RavlN {
  RAVL_INITVIRTUALCONSTRUCTOR(bodyname) \
  RAVL_VIRTUALCONSTRUCTOR_HANDLE(bodyname,handlename,basename)
 
-  // Initalise the virtual constructor and the handle
+  // Initialise the virtual constructor and the handle
 
 #define RAVL_INITVIRTUALCONSTRUCTOR_FULL_NAMED(bodyname,handlename,basename,name) \
  RAVL_INITVIRTUALCONSTRUCTOR_NAMED(bodyname,name) \
