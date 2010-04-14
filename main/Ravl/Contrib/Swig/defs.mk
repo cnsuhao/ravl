428eba7886c4e599072d9eb51a92e0ca6a5f8ef5
robowaz <robowaz@7226f50c-d813-0410-99bf-979b288ab488>
2010-04-14 16:52:27 +0000

    Added RLog Swig interface.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@7692 7226f50c-d813-0410-99bf-979b288ab488

--- main/Ravl/Contrib/Swig/defs.mk
+++ main/Ravl/Contrib/Swig/defs.mk
@@ -10,7 +10,7 @@ PACKAGE=Ravl/Swig
 
 HEADERS=Size.i Types.i Point2d.i Polygon2d.i Image.i IndexRange2d.i Index2d.i Array2d.i Index.i String.i \
  IO.i Font.i SArray1d.i Affine2d.i DList.i Date.i PointSet2d.i RealRange.i RealRange2d.i Hash.i DPIPort.i \
- SmartPtr.i
+ SmartPtr.i RLog.i
 
 SOURCES=
 
