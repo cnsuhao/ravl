c92ddb5f364ddc5325a3aa6db705f4d08af7b6ad
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2011-03-31 09:10:35 +0000

    Doc update
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@8015 7226f50c-d813-0410-99bf-979b288ab488

--- main/Ravl/PatternRec/SVM/KernelFunc.hh
+++ main/Ravl/PatternRec/SVM/KernelFunc.hh
@@ -15,7 +15,7 @@
 //! lib =       RavlSVM
 //! date =      "3/07/03"
 //! rcsid =     "$Id: KernelFunc.hh 7752 2006-01-31 14:46:56Z alex $"
-//! docentry =  "Ravl.API.Pattern_Recognition.Classifier.SVM"
+//! docentry =  "Ravl.API.Pattern_Recognition.Classifier.SVM.Kernel functions"
 //////////////////////////////////////////////////////////////////////////////
 
 #include "Ravl/SArray1d.hh"
