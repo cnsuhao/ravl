0aa4dbff9950f033996b356a8fa3f91d8000c252
ees1wc <ees1wc@7226f50c-d813-0410-99bf-979b288ab488>
2011-06-30 12:54:31 +0000

    Added docentry etc.
    git-svn-id: https://ravl.svn.sourceforge.net/svnroot/ravl@8074 7226f50c-d813-0410-99bf-979b288ab488

--- main/Ravl/3D/CameraCal/PinholeCamera1.hh
+++ main/Ravl/3D/CameraCal/PinholeCamera1.hh
@@ -1,3 +1,13 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+//! lib=RavlCameraCal
+//! author="Joel Mitchelson"
+//! docentry="Ravl.API.3D.Camera Calibration"
+//! file="Ravl/3D/CameraCal/PinholeCamera1.hh"
 #ifndef _PINHOLE_CAMERA_1_HH_
 #define _PINHOLE_CAMERA_1_HH_
 
