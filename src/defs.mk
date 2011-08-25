f34740a0ae8ead58f6788f9883b0517e8310003a
John Field <John.Field@surrey.ac.uk>
2011-08-25 18:40:10 +0100

    Move to using QMake as external package          Remove embedded QMake          Require external QMake package in QMake directory parallel                with the Ravl 'src' directory; cloning QMake from its                repository, git.code.sf.net/p/quickmake/code          Call QMake configure script from configure          Build "Staging QMake" for use in building Ravl plus                a "Release" QMake for installation on the local box

--- src/defs.mk
+++ src/defs.mk
@@ -1,5 +1,5 @@
 # This file is part of RAVL, Recognition And Vision Library 
-# Copyright (C) 2001, University of Surrey
+# Copyright (C) 2001-11, University of Surrey
 # This code may be redistributed under the terms of the GNU Lesser
 # General Public License (LGPL). See the lgpl.licence file for details or
 # see http://www.gnu.org/copyleft/lesser.html
@@ -11,7 +11,7 @@ PACKAGE=Ravl
 
 DESCRIPTION=RAVL, Recognition and Vision Library
 
-NESTED=QMake.r Core.r OS.r CCMath.r Math.r PatternRec.r Audio.r \
+NESTED=Core.r OS.r CCMath.r Math.r PatternRec.r Audio.r \
 Image.r 3D.r Logic.r SourceTools.r GUI.r CompVision.r Contrib.r Applications.r 
 
 LOCAL_FILES=install lgpl.license gpl.license README
