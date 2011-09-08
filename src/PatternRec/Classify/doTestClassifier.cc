b22844ae37f90a7b6ab701aa038fcd52459bd4c3
Charles Galambos <c.galambos@omniperception.com>
2011-09-08 10:01:55 +0100

    Don't use RLog within RAVL its an optional library.

--- src/PatternRec/Classify/doTestClassifier.cc
+++ src/PatternRec/Classify/doTestClassifier.cc
@@ -1,6 +1,6 @@
 #include "Ravl/Option.hh"
 #include "Ravl/XMLFactory.hh"
-#include "Ravl/RLog.hh"
+#include "Ravl/OS/SysLog.hh"
 #include "Ravl/Resource.hh"
 #include "Ravl/PatternRec/DesignClassifierSupervised.hh"
 #include "Ravl/PatternRec/DataSetVectorLabel.hh"
@@ -22,32 +22,26 @@ int main(int nargs, char **argv) {
   RavlN::SetResourceRoot(installDir);
   StringC classifierFile = opts.String("classifier", "", "Load the trained classifier from this file.");
   StringC dsetFile = opts.String("dset", "", "The dataset to perform the test on!");
-  bool verbose = opts.Boolean("v", false, "Verbose mode.");
-  StringC logFile = opts.String("l", "stderr", "Checkpoint log file. ");
-  StringC logLevel = opts.String("ll", "debug", "Logging level (debug, info, warning, error)");
-
+  //bool verbose = opts.Boolean("v", false, "Verbose mode.");
   opts.Check();
-
-  RLogInit(nargs, argv, logFile.chars(), verbose);
-  RLogSubscribeL(logLevel.chars());
 
   try {
 
     // And save the classifier
     ClassifierC classifier;
-    rInfo("Loading classifier from '%s'", classifierFile.data());
+    SysLog(SYSLOG_INFO,"Loading classifier from '%s'", classifierFile.data());
     if (!Load(classifierFile, classifier)) {
-      rError("Trouble loading classifier");
+      SysLog(SYSLOG_ERR,"Trouble loading classifier");
       return 1;
     }
 
     // Get dataset
-    rInfo("Loading dataset from file '%s'", dsetFile.data());
+    SysLog(SYSLOG_INFO,"Loading dataset from file '%s'", dsetFile.data());
     // FIXME: Want to use Load/Save instead
     DataSetVectorLabelC testDataSet;
     IStreamC is(dsetFile);
     if (!is.good()) {
-      rError("Trouble loading dataset from file!");
+      SysLog(SYSLOG_ERR,"Trouble loading dataset from file!");
       return 1;
     }
     is >> testDataSet;
@@ -55,11 +49,11 @@ int main(int nargs, char **argv) {
     // Lets get error on the test data set
     ErrorC error;
     RealT pmc = error.Error(classifier, testDataSet);
-    rInfo("The probability of miss-classification is %0.4f ", pmc);
+    SysLog(SYSLOG_INFO,"The probability of miss-classification is %0.4f ", pmc);
 
   } catch (const RavlN::ExceptionC &exc) {
-    rError("Exception:%s", exc.Text());
+    SysLog(SYSLOG_ERR,"Exception:%s", exc.Text());
   } catch (...) {
-    rError("Unknown exception");
+    SysLog(SYSLOG_ERR,"Unknown exception");
   }
 }
