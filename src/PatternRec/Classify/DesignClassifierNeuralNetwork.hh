a0a8df9f69e37149d9a7c4bc01b8ebeb93b4c564
Kieron Messer <k.messer@omniperception.com>
2011-09-20 14:52:36 +1000

    Allow construction from XML factory

--- src/PatternRec/Classify/DesignClassifierNeuralNetwork.hh
+++ src/PatternRec/Classify/DesignClassifierNeuralNetwork.hh
@@ -12,119 +12,126 @@
 //! file="Ravl/PatternRec/Classify/DesignClassifierGaussianMixture.hh"
 
 #include "Ravl/PatternRec/DesignClassifierSupervised.hh"
+#include "Ravl/XMLFactory.hh"
 
 namespace RavlN {
-  
+
   //! userlevel=Develop
   //: Design a gaussian mixture classifier .
-  
-  class DesignClassifierNeuralNetworkBodyC
-    : public DesignClassifierSupervisedBodyC
-  {
-  public:
-    DesignClassifierNeuralNetworkBodyC(UIntT nLayers, UIntT nInputs, UIntT nHidden, UIntT nOutputs);
-    //: Constructor.
-    
-    DesignClassifierNeuralNetworkBodyC(istream &strm);
-    //: Load from stream.
-    
-    DesignClassifierNeuralNetworkBodyC(BinIStreamC &strm);
-    //: Load from binary stream.
-    
-    virtual bool Save (ostream &out) const;
-    //: Writes object to stream, can be loaded using constructor
-    
-    virtual bool Save (BinOStreamC &out) const;
-    //: Writes object to stream, can be loaded using constructor
 
-    virtual ClassifierC Apply(const SampleC<VectorC> &in,const SampleC<UIntT> &out);
-    //: Train a neural network.  The number of input dimensions must be the same as specified in the
-    //: constructor.  The number of different labels must be the same as the number of outputs.  Also
-    //: the output labels must run from 0 to nOutputs-1.
-    
-    virtual ClassifierC Apply(const SampleC<VectorC> &in,const SampleC<UIntT> &out,const SampleC<RealT> &weight);
-    //: Create a clasifier with weights for the samples.
-    
-  protected:
-    UIntT m_nLayers;
-    UIntT m_nInputs;
-    UIntT m_nHidden;
-    UIntT m_nOutputs;
+  class DesignClassifierNeuralNetworkBodyC: public DesignClassifierSupervisedBodyC {
+    public:
+      DesignClassifierNeuralNetworkBodyC(UIntT nLayers, UIntT nInputs, UIntT nHidden, UIntT nOutputs);
+      //: Constructor.
+
+      DesignClassifierNeuralNetworkBodyC(const XMLFactoryContextC & factory);
+      //: factory constructor
+
+      DesignClassifierNeuralNetworkBodyC(istream &strm);
+      //: Load from stream.
+
+      DesignClassifierNeuralNetworkBodyC(BinIStreamC &strm);
+      //: Load from binary stream.
+
+      virtual bool Save(ostream &out) const;
+      //: Writes object to stream, can be loaded using constructor
+
+      virtual bool Save(BinOStreamC &out) const;
+      //: Writes object to stream, can be loaded using constructor
+
+      virtual ClassifierC Apply(const SampleC<VectorC> &in, const SampleC<UIntT> &out);
+      //: Train a neural network.  The number of input dimensions must be the same as specified in the
+      //: constructor.  The number of different labels must be the same as the number of outputs.  Also
+      //: the output labels must run from 0 to nOutputs-1.
+
+      virtual ClassifierC Apply(const SampleC<VectorC> &in, const SampleC<UIntT> &out, const SampleC<RealT> &weight);
+      //: Create a clasifier with weights for the samples.
+
+    protected:
+      UIntT m_nLayers;
+      UIntT m_nInputs;
+      UIntT m_nHidden;
+      UIntT m_nOutputs;
   };
-  
+
   //! userlevel=Normal
   //: Design a gaussian mixture classifier .
 
-  class DesignClassifierNeuralNetworkC
-    : public DesignClassifierSupervisedC
-  {
-  public:
-    DesignClassifierNeuralNetworkC()
-    {}
-    //: Default constructor.
-    // Creates an invalid handle.
-    
-    DesignClassifierNeuralNetworkC(UIntT nLayers, UIntT nInputs, UIntT nHidden, UIntT nOutputs)
-      : DesignClassifierSupervisedC(*new DesignClassifierNeuralNetworkBodyC(nLayers, nInputs, nHidden, nOutputs))
-    {}
-    //: Create a new designer.
-    
-    DesignClassifierNeuralNetworkC(istream &strm);
-    //: Load from stream.
-    
-    DesignClassifierNeuralNetworkC(BinIStreamC &strm);
-    //: Load from binary stream.
+  class DesignClassifierNeuralNetworkC: public DesignClassifierSupervisedC {
+    public:
+      DesignClassifierNeuralNetworkC() {
+      }
+      //: Default constructor.
+      // Creates an invalid handle.
 
-  protected:
-    DesignClassifierNeuralNetworkC(DesignClassifierNeuralNetworkBodyC &bod)
-      : DesignClassifierSupervisedC(bod)
-    {}
-    //: Body constructor.
+      DesignClassifierNeuralNetworkC(UIntT nLayers, UIntT nInputs, UIntT nHidden, UIntT nOutputs)
+          : DesignClassifierSupervisedC(*new DesignClassifierNeuralNetworkBodyC(nLayers, nInputs, nHidden, nOutputs)) {
+      }
+      //: Create a new designer.
 
-    DesignClassifierNeuralNetworkC(DesignClassifierNeuralNetworkBodyC *bod)
-      : DesignClassifierSupervisedC(bod)
-    {}
-    //: Body ptr constructor.
-    
-    DesignClassifierNeuralNetworkBodyC &Body()
-    { return static_cast<DesignClassifierNeuralNetworkBodyC &>(DesignClassifierSupervisedC::Body()); }
-    //: Access body.
-    
-    const DesignClassifierNeuralNetworkBodyC &Body() const
-    { return static_cast<const DesignClassifierNeuralNetworkBodyC &>(DesignClassifierSupervisedC::Body()); }
-    //: Access body.
-    
-  public:
-    
+      DesignClassifierNeuralNetworkC(const XMLFactoryContextC &factory)
+         :  DesignClassifierSupervisedC(*new DesignClassifierNeuralNetworkBodyC(factory))
+       {}
+       //: Construct from XML factory
+
+      DesignClassifierNeuralNetworkC(istream &strm);
+      //: Load from stream.
+
+      DesignClassifierNeuralNetworkC(BinIStreamC &strm);
+      //: Load from binary stream.
+
+    protected:
+      DesignClassifierNeuralNetworkC(DesignClassifierNeuralNetworkBodyC &bod)
+          : DesignClassifierSupervisedC(bod) {
+      }
+      //: Body constructor.
+
+      DesignClassifierNeuralNetworkC(DesignClassifierNeuralNetworkBodyC *bod)
+          : DesignClassifierSupervisedC(bod) {
+      }
+      //: Body ptr constructor.
+
+      DesignClassifierNeuralNetworkBodyC &Body() {
+        return static_cast<DesignClassifierNeuralNetworkBodyC &>(DesignClassifierSupervisedC::Body());
+      }
+      //: Access body.
+
+      const DesignClassifierNeuralNetworkBodyC &Body() const {
+        return static_cast<const DesignClassifierNeuralNetworkBodyC &>(DesignClassifierSupervisedC::Body());
+      }
+      //: Access body.
+
+    public:
+
   };
-  
-  inline istream &operator>>(istream &strm,DesignClassifierNeuralNetworkC &obj) {
+
+  inline istream &operator>>(istream &strm, DesignClassifierNeuralNetworkC &obj) {
     obj = DesignClassifierNeuralNetworkC(strm);
     return strm;
   }
   //: Load from a stream.
   // Uses virtual constructor.
-  
-  inline ostream &operator<<(ostream &out,const DesignClassifierNeuralNetworkC &obj) {
+
+  inline ostream &operator<<(ostream &out, const DesignClassifierNeuralNetworkC &obj) {
     obj.Save(out);
     return out;
   }
   //: Save to a stream.
   // Uses virtual constructor.
-  
-  inline BinIStreamC &operator>>(BinIStreamC &strm,DesignClassifierNeuralNetworkC &obj) {
+
+  inline BinIStreamC &operator>>(BinIStreamC &strm, DesignClassifierNeuralNetworkC &obj) {
     obj = DesignClassifierNeuralNetworkC(strm);
     return strm;
   }
   //: Load from a binary stream.
   // Uses virtual constructor.
-  
-  inline BinOStreamC &operator<<(BinOStreamC &out,const DesignClassifierNeuralNetworkC &obj) {
+
+  inline BinOStreamC &operator<<(BinOStreamC &out, const DesignClassifierNeuralNetworkC &obj) {
     obj.Save(out);
     return out;
   }
-  //: Save to a stream.
-  // Uses virtual constructor.
+//: Save to a stream.
+// Uses virtual constructor.
 
 }
 
