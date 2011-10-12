0303e95538bcdf7d1a7af29a0b26135d213c6724
Alex Kostin <a.kostin@omniperception.com>
2011-10-12 18:00:15 +0100

    Added floating point constant suffixes to reduce type conversions and compiler warnings

--- src/Image/Base/YPbPrBT709Value.cc
+++ src/Image/Base/YPbPrBT709Value.cc
@@ -7,17 +7,17 @@ namespace RavlImageN {
   // Convert a pixel type from RGB to YPbPrBT709
   
   void RGBFloat2YPbPrBT709Float(const RGBValueC<float> &value,YPbPrBT709ValueC<float> &outValue) {
-    outValue.Set( + 0.21259    * value.Red() + 0.71521    * value.Green() + 0.07220    * value.Blue(),
-                  - 0.11719 * value.Red() - 0.39423 * value.Green() + 0.51141      * value.Blue(),
-                  + 0.51141      * value.Red() - 0.46454 * value.Green() - 0.04688 * value.Blue());
+    outValue.Set( + 0.21259f    * value.Red() + 0.71521f * value.Green() + 0.07220f * value.Blue(),
+                  - 0.11719f    * value.Red() - 0.39423f * value.Green() + 0.51141f * value.Blue(),
+                  + 0.51141f    * value.Red() - 0.46454f * value.Green() - 0.04688f * value.Blue());
   }
   
   // Convert a pixel type from YPbPrBT709 to RGB
   
   void YPbPrBT709Float2RGBFloat(const YPbPrBT709ValueC<float> &value,RGBValueC<float> &outValue) {
-    outValue.Set(value.Y() + 0.00000 * value.Pb() + 1.53967    * value.Pr(),
-                 value.Y() - 0.18317    * value.Pb() - 0.45764    * value.Pr(),
-                 value.Y() + 1.81421     * value.Pb() + 0.00000 * value.Pr());
+    outValue.Set(value.Y() + 0.00000f * value.Pb() + 1.53967f * value.Pr(),
+                 value.Y() - 0.18317f * value.Pb() - 0.45764f * value.Pr(),
+                 value.Y() + 1.81421f * value.Pb() + 0.00000f * value.Pr());
   }
   
      
