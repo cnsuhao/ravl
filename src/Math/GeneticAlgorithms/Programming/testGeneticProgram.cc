5288dd26265d6f9501201be287c2581c6cd71ff8
Charles Galambos <c.galambos@omniperception.com>
2012-01-05 20:15:54 +0000

    Update test program.

--- src/Math/GeneticAlgorithms/Programming/testGeneticProgram.cc
+++ src/Math/GeneticAlgorithms/Programming/testGeneticProgram.cc
@@ -1,24 +1,39 @@
 
 #include "Ravl/Genetic/GPVariable.hh"
+#include "Ravl/Genetic/GPInstruction.hh"
+#include "Ravl/UnitTest.hh"
 
-int testVariable();
+int testGeneInstIO();
 
 int main(int nargs,char **argv)
 {
   int ln = 0;
-  if((ln = testVariable()) != 0) {
+  if((ln = testGeneInstIO()) != 0) {
     std::cerr <<"Test failed on line " << ln << "\n";
     return 1;
   }
   return 0;
 }
 
+using RavlN::GeneticN::GenomeC;
+using RavlN::GeneticN::GeneFactoryC;
+using RavlN::GeneticN::GPInstructionC;
 
-int testVariable()
+int testGeneInstIO()
 {
-  GPVariableC<int> var1;
-  GPVariableC<int> var2;
+  for(unsigned i = 0;i < 10000;i++) {
 
+    GenomeC::RefT genome = new GenomeC(*RavlN::GeneticN::InstructionGeneType());
 
+    // Instantiate genome
+    GeneFactoryC factory(*genome);
+    GPInstructionC::RefT inst;
+    factory.Get(inst);
+    RAVL_TEST_TRUE(inst.IsValid());
+
+    GenomeC::RefT genomeRL;
+    if(!TestBinStreamIO(genome,genomeRL))
+      return __LINE__;
+  }
   return 0;
 }
