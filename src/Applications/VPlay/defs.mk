2a04f2eea1b4734cf7dadc6918be91a861129ea8
Charles Galambos <c.galambos@omniperception.com>
2012-02-23 09:54:59 +0000

    Fix libs.

--- src/Applications/VPlay/defs.mk
+++ src/Applications/VPlay/defs.mk
@@ -30,8 +30,9 @@ PROGLIBS =  RavlImgIOV4L.opt RavlExtImgIO.opt RavlDPDisplay \
  RavlImageIO RavlVideoIO  CSPDriver.opt RavlURLIO.opt RavlLibMPEG2.opt \
  RavlImageProc RavlNet RavlDVDRead.opt RavlAVIFile.opt \
  RavlImgIOJasper.opt RavlDV.opt  RavlImgIOV4L2.opt \
- RavlRawVidIO.opt RavlLibFFmpeg.opt RavlImgIO1394dc.opt \
- OmniSanyoVCCHD4000P.opt  OmniFlashCamera OmniImageImgIOProsilica 
+ RavlRawVidIO.opt RavlLibFFmpeg.opt RavlImgIO1394dc.opt 
+# OmniSanyoVCCHD4000P.opt 
+# OmniFlashCamera OmniImageImgIOProsilica 
         
   
 # RavlImgIOuEye.opt
