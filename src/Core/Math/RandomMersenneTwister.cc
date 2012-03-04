78cf97b4d07bf532b2ab8433434e11e4acb49d37
Charles Galambos <craftit@gmail.com>
2012-03-04 16:58:29 +0000

    Add method to seed from string.

--- src/Core/Math/RandomMersenneTwister.cc
+++ src/Core/Math/RandomMersenneTwister.cc
@@ -18,6 +18,7 @@
 // All rights reserved.
 
 #include "Ravl/RandomMersenneTwister.hh"
+#include "Ravl/String.hh"
 
 /* Period parameters */  
 #define N 624
@@ -42,8 +43,7 @@ namespace RavlN {
   void RandomMersenneTwisterC::Seed(UInt32T s) {
     mt[0]= s & 0xffffffffUL;
     for (mti=1; mti<N; mti++) {
-      mt[mti] = 
-	(1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
+      mt[mti] = (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti);
       /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
       /* In the previous versions, MSBs of the seed affect   */
       /* only MSBs of the array mt[].                        */
@@ -56,28 +56,37 @@ namespace RavlN {
   /* initialize by an array with array-length */
   /* init_key is the array for initializing keys */
   /* key_length is its length */
-  void RandomMersenneTwisterC::Seed(UInt32T init_key[], UInt32T key_length) {
+  void RandomMersenneTwisterC::Seed(const UInt32T init_key[], UInt32T key_length) {
     unsigned int i, j, k;
     Seed(19650218UL);
     i=1; j=0;
     k = (N>key_length ? N : key_length);
     for (; k; k--) {
-      mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL))
-	+ init_key[j] + j; /* non linear */
+      mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL)) + init_key[j] + j; /* non linear */
       mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
       i++; j++;
       if (i>=N) { mt[0] = mt[N-1]; i=1; }
       if (j>=key_length) j=0;
     }
     for (k=N-1; k; k--) {
-      mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL))
-	- i; /* non linear */
+      mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL)) - i; /* non linear */
       mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
       i++;
       if (i>=N) { mt[0] = mt[N-1]; i=1; }
     }
 
     mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
+  }
+
+  //: Initialises the random number generator with the passed character array
+
+  void RandomMersenneTwisterC::Seed(const char *str,UInt32T key_len) {
+    // FIXME:- Move
+    StringC randomSeed(str,key_len,key_len);
+    unsigned pad = 4 - (randomSeed.Size() % sizeof(unsigned));
+    for(unsigned k = 0;k < pad;k++)
+      randomSeed += ' ';
+    Seed(reinterpret_cast<const unsigned *>(randomSeed.chars()),randomSeed.Size()/sizeof(unsigned));
   }
 
   /* generates a random number on [0,0xffffffff]-interval */
@@ -91,15 +100,15 @@ namespace RavlN {
       unsigned int kk;
 
       if (mti == N+1)   /* if init_genrand() has not been called, */
-	Seed(5489UL); /* a default initial seed is used */
+        Seed(5489UL); /* a default initial seed is used */
 
       for (kk=0;kk<N-M;kk++) {
-	y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-	mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+        mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
       }
       for (;kk<N-1;kk++) {
-	y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-	mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+        mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
       }
       y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
       mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
