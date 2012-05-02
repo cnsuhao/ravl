f597f8d5881106f8f0d770f18a34516d93255fed
Charles Galambos <c.galambos@omniperception.com>
2012-05-02 18:25:01 +0100

    Include some copyright messages.

--- src/Contrib/XMPP/iksemel/IksemelConnection.hh
+++ src/Contrib/XMPP/iksemel/IksemelConnection.hh
@@ -1,10 +1,15 @@
+// This file is part of RAVL, Recognition And Vision Library
+// Copyright (C) 2010, OmniPerception Ltd.
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
 /* 
  * File:   IksemelConnection.hh
  * Author: charlesgalambos
  *
  * Created on November 14, 2010, 12:42 PM
  */
-
 #ifndef RAVL_XMPP_IKSEMEL_HH
 #define	RAVL_XMPP_IKSEMEL_HH
 
