ae1dc0393082183404403652b8d77eb613572b57
John Field <John.Field@surrey.ac.uk>
2012-07-30 15:01:55 +0100

    Use things from std namespace explicitly, fix warning from new eclipse    Applying commit e36a46fac65e63d46b801a044385da4dfff3e706 from Ravl repo

--- QMake/src/Support/fixstdnamespace.sh
+++ QMake/src/Support/fixstdnamespace.sh
@@ -9,5 +9,7 @@ perl -pi -e 's/ endl;/ std::endl;/g' *.cc *.hh
 perl -pi -e 's/type_info/std::type_info/g' *.cc *.hh
 perl -pi -e 's/ cerr/ std::cerr/g' *.cc *.hh
 perl -pi -e 's/ cout/ std::cout/g' *.cc *.hh
+perl -pi -e 's/\(cerr/\(std::cerr/g' *.cc *.hh
+perl -pi -e 's/\(cout/\(std::cout/g' *.cc *.hh
 perl -pi -e 's/std::std::/std::/g' *.cc *.hh
 
