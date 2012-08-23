062d5555e4205b4bfa5b4a1b0c7f250e3ba7bf40
Charles Galambos <c.galambos@omniperception.com>
2012-08-23 09:45:43 +0100

    Fix build

--- src/PatternRec/Classify/DesignCascadeBinary.cc
+++ src/PatternRec/Classify/DesignCascadeBinary.cc
@@ -19,7 +19,7 @@
 #include "Ravl/PatternRec/DataSet2Iter.hh"
 #include "Ravl/PatternRec/DataSet3Iter.hh"
 #include "Ravl/PatternRec/FuncSubset.hh"
-#include "Ravl/Face/Roc.hh"
+//#include "Ravl/Face/Roc.hh"
 #include "Ravl/PatternRec/ClassifierCascade.hh"
 #include "Ravl/PatternRec/Error.hh"
 #include "Ravl/PatternRec/FeatureSelectPlusLMinusR.hh"
