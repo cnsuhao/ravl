73dec2200b92813960d37894597f88b301c159c7
John Field <John.Field@surrey.ac.uk>
2012-09-19 16:41:56 +0100

    Vary what MUSLINK links in based on available libs

--- src/Image/ExternalImageIO/Core/defs.mk
+++ src/Image/ExternalImageIO/Core/defs.mk
@@ -18,6 +18,22 @@ HEADERS = ExtImgIO.hh
 
 MUSTLINK =  ExtImgIO.cc
 
+# Set what functionality the MUSTLINK module needs to initialise
+ifeq (JPEG, $(filter JPEG,$(RESOURCES)))
+  CFLAGS+=-DMUSTLINK_JPEG
+  CCFLAGS+=-DMUSTLINK_JPEG
+endif
+
+ifeq (libPNG, $(filter libPNG,$(RESOURCES)))
+  CFLAGS+=-DMUSTLINK_PNG
+  CCFLAGS+=-DMUSTLINK_PNG
+endif
+
+ifeq (LibTIFF, $(filter LibTIFF,$(RESOURCES)))
+  CFLAGS+=-DMUSTLINK_TIFF
+  CCFLAGS+=-DMUSTLINK_TIFF
+endif
+
 EXAMPLES = exExtImgIO.cc exImgMemIO.cc
 
 USESLIBS = RavlImage RavlIO RavlImageIO LibJPEG LibPNG LibTIFF
