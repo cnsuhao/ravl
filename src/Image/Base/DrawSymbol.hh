5fceea662eaf065b5da8ce78627bd470e81aeaf8
Bill Christmas <w.christmas@surrey.ac.uk>
2013-03-25 18:13:08 +0000

    Added DrawText funcs with default fonts Added header to include all the other drawing headers

---
+++ src/Image/Base/DrawSymbol.hh
@@ -0,0 +1,28 @@
+// This file is part of RAVL, Recognition And Vision Library 
+// Copyright (C) 2001, University of Surrey
+// This code may be redistributed under the terms of the GNU Lesser
+// General Public License (LGPL). See the lgpl.licence file for details or
+// see http://www.gnu.org/copyleft/lesser.html
+// file-header-ends-here
+#ifndef RAVLIMAGE_DRAWSYMBOL_HEADER
+#define RAVLIMAGE_DRAWSYMBOL_HEADER 1
+///////////////////////////////////////////////////////////////////
+//! rcsid="$Id$"
+//! author="Bill Christmas"
+//! date="25/3/2013"
+//! docentry="Ravl.API.Images.Drawing"
+//! lib=RavlImage
+//! userlevel=Normal
+//! file="Ravl/Image/Base/DrawSymbol.hh"
+//! example=exDraw.cc
+
+#include "DrawCircle.hh"
+#include "DrawCross.hh"
+#include "DrawEllipse.hh"
+#include "DrawFrame.hh"
+#include "DrawLine.hh"
+#include "DrawMask.hh"
+#include "DrawPolygon.hh"
+#include "Font.hh"
+
+#endif
