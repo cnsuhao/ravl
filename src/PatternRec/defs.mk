c2a8eb12282ea525752ac9117d3fb83a61468984
Charles Galambos <craftit@gmail.com>
2013-07-09 11:10:58 +0100

    Add FileIDX.

--- src/PatternRec/defs.mk
+++ src/PatternRec/defs.mk
@@ -9,7 +9,7 @@
 
 PACKAGE=Ravl
 
-NESTED=fann.r DataSet.r Optimise.r Distance.r Cluster.r Modeling.r Classify.r \
+NESTED=FileIDX.r fann.r DataSet.r Optimise.r Distance.r Cluster.r Modeling.r Classify.r \
  FeatureSelection.r Performance.r IO.r HiddenMarkovModels.r KalmanFilter.r Arff.r SVM.r
 
 
