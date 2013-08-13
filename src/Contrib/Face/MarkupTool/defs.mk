6e2be10d91353ddae5fad44529e41ad68dd0af0c
John Field <John.Field@surrey.ac.uk>
2013-08-13 14:35:18 +0100

    Tidy defs.mk

--- src/Contrib/Face/MarkupTool/defs.mk
+++ src/Contrib/Face/MarkupTool/defs.mk
@@ -4,13 +4,6 @@
 # General Public License (LGPL). See the lgpl.licence file for details or
 # see http://www.gnu.org/copyleft/lesser.html
 # file-header-ends-here
-#    * MAINS A list of programs to compile.
-#    * SOURCES Source files to compile.
-#    * HEADERS Header files needed for this source code.
-#    * PLIB Name of library to create.
-#    * USESLIBS Libraries needed to compile source code.
-#    * NESTED Subdirectories to compile.
-#! file="OmniSoft/Applications/WhoFIT/defs.mk"
 
 REQUIRES=RLog
 
