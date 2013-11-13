4432915b59c412b895c4248db372c6f75e3339dd
John Field <John.Field@surrey.ac.uk>
2013-11-13 10:43:38 +0000

    Should include CUDA.def

--- src/Core/Defs/defs.mk
+++ src/Core/Defs/defs.mk
@@ -8,7 +8,7 @@
 
 DESCRIPTION= Defintion files for external libraries that may be used in RAVL.  
 
-EXTERNALLIBS= GTK.def LibPNG.def posixThreads.def zlib.def \
+EXTERNALLIBS= CUDA.def GTK.def LibPNG.def posixThreads.def zlib.def \
  DynLink.def LibTIFF.def OpenGL.def libGL.def libGLU.def LibJPEG.def \
  Socket.def x11.def AudioFile.def libDepends formatInfo libglade.def \
  RLog.def GLUT.def ObjC.def RT.def FreeType2.def Math.def \
