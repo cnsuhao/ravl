fda93d44921a7d7ad3e3b9d8608f0e3cacd39e39
John Field <John.Field@surrey.ac.uk>
2014-02-25 15:09:30 +0000

    Only link single Ravl with external libs it needs

--- src/Single/defs.mk
+++ src/Single/defs.mk
@@ -1,6 +1,12 @@
+# This file is part of RAVL, Recognition And Vision Library 
+# Copyright (C) 2012-14, University of Surrey
+# This code may be redistributed under the terms of the GNU Lesser
+# General Public License (LGPL). See the lgpl.licence file for details or
+# see http://www.gnu.org/copyleft/lesser.html
+#
+# file-header-ends-here
 
-LIBS=$(ALL_EXTERNAL_LIBS)
 
-PLIBDEPENDS=$(patsubst lib%,%,$(SINGLE_RECIPE))
+USESLIBS=$(patsubst lib%,%,$(SINGLE_RECIPE))
 
 SINGLESO=libRavl
