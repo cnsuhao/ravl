b1fe2a6b2bfea46ea53de3aa65567aed702691c4
John Field <John.Field@surrey.ac.uk>
2014-10-15 18:21:47 +0100

    Drop locally set -g

--- src/Image/Processing/Segmentation/defs.mk
+++ src/Image/Processing/Segmentation/defs.mk
@@ -1,11 +1,9 @@
 # This file is part of RAVL, Recognition And Vision Library 
-# Copyright (C) 2001, University of Surrey
+# Copyright (C) 2001-14, University of Surrey
 # This code may be redistributed under the terms of the GNU Lesser
 # General Public License (LGPL). See the lgpl.licence file for details or
 # see http://www.gnu.org/copyleft/lesser.html
 # file-header-ends-here
-#! rcsid="$Id$"
-#! file="Ravl/Image/Processing/Segmentation/defs.mk"
 
 PACKAGE=Ravl/Image
 
@@ -17,8 +15,6 @@ HEADERS=Segmentation.hh RegionSet.hh ConnectedComponents.hh \
 SOURCES=ChromaThreshold.cc Segmentation.cc ConnectedComponents.cc \
  BlueScreen.cc SegmentExtrema.cc PixelSelectStack.cc 
 
-CCPPFLAGS += -g
-LDFLAGS += -g
 MAINS=extrema.cc
 
 PLIB=RavlImageProc
