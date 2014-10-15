914bd0ee88c26b424836fffc2032d5bf3c0080d3
John Field <John.Field@surrey.ac.uk>
2014-10-15 11:24:44 +0100

    Drop unused entires in USESLIBS

--- src/Contrib/DV/defs.mk
+++ src/Contrib/DV/defs.mk
@@ -1,11 +1,9 @@
 # This file is part of RAVL, Recognition And Vision Library 
-# Copyright (C) 2001, University of Surrey
+# Copyright (C) 2001-14, University of Surrey
 # This code may be redistributed under the terms of the GNU Lesser
 # General Public License (LGPL). See the lgpl.licence file for details or
 # see http://www.gnu.org/copyleft/lesser.html
 # file-header-ends-here
-#! rcsid="$Id$"
-#! file="Ravl/Contrib/DV/defs.mk"
 
 PACKAGE = Ravl/Image
 
@@ -25,7 +23,7 @@ PLIB=RavlDV
 
 EXTERNALLIBS= LibDV.def
 
-USESLIBS=system RavlCore RavlImage RavlIO RavlOS RavlOSIO glib GTK LibDV  RavlVideoIO RavlAV
+USESLIBS=system RavlCore RavlImage RavlIO RavlOS RavlOSIO LibDV  RavlVideoIO RavlAV
 
 PROGLIBS= RavlDPDisplay.opt RavlDPDisplay RavlDevAudio
 
