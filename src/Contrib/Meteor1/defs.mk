1cb7421344221ef68b9110bb5efc9ec7fd65b8b7
John Field <John.Field@surrey.ac.uk>
2014-10-15 12:03:09 +0100

    Protect use of external library with a REQUIRES

--- src/Contrib/Meteor1/defs.mk
+++ src/Contrib/Meteor1/defs.mk
@@ -1,11 +1,9 @@
 # This file is part of RAVL, Recognition And Vision Library 
-# Copyright (C) 2001, University of Surrey
+# Copyright (C) 2001-14, University of Surrey
 # This code may be redistributed under the terms of the GNU Lesser
 # General Public License (LGPL). See the lgpl.licence file for details or
 # see http://www.gnu.org/copyleft/lesser.html
 # file-header-ends-here
-#! rcsid="$Id$"
-#! file="Ravl/Contrib/Meteor1/defs.mk"
 #! author="Charles Galambos"
 
 DESCRIPTION = Meteor driver
@@ -34,5 +32,5 @@ USESLIBS = RavlImageIO
 
 PROGLIBS = RavlGUI RavlGUI2D
 
-REQUIRES = libGTK2
+REQUIRES = libGTK2 Meteor1
 
