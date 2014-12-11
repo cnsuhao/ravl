f6cedc9a23e790fadca4c6011540a0bcbe70caf7
Bill Christmas <w.christmas@surrey.ac.uk>
2014-12-11 10:46:33 +0000

    Moved GPSCoordinate to Math/Geometry/Euclidean/3D

--- src/Core/Math/defs.mk
+++ src/Core/Math/defs.mk
@@ -17,15 +17,13 @@ MAINS =
 
 # HEADERS = all include files of this project to be exported
 HEADERS = StdMath.hh Random.hh RandomGauss.hh Angle.hh ScalMath.hh \
- TFMatrix.hh RandomMersenneTwister.hh LogValue.hh RankedScoreArray.hh \
- GPSCoordinate.hh
+ TFMatrix.hh RandomMersenneTwister.hh LogValue.hh RankedScoreArray.hh
 
 
 # SOURCES = all C modules, not containing main separated by space. The compiled
 #   code of these modules is stored in the project library.
 SOURCES = StdMath.cc Random.cc RandomGauss.cc Angle.cc ScalMath.cc \
- RandomMersenneTwister.cc Erf.cc LogValue.cc \
- GPSCoordinate.cc
+ RandomMersenneTwister.cc Erf.cc LogValue.cc
 
 # EMGRavl.cc UsefulFn.cc Erf.cc
 
