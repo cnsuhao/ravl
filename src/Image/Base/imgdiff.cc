07792c848d4a22206a3d33ac2b59c19682dcf474
Bill Christmas <w.christmas@surrey.ac.uk>
2015-04-22 16:20:45 +0100

    Added basic greyscale value manipulation to imgdiff o/p

--- src/Image/Base/imgdiff.cc
+++ src/Image/Base/imgdiff.cc
@@ -18,6 +18,7 @@
 #include "Ravl/IO.hh"
 #include "Ravl/Image/Image.hh"
 #include "Ravl/Array2dIter3.hh"
+#include "Ravl/Array2dIter.hh"
 
 using namespace RavlN;
 using namespace RavlImageN;
@@ -25,8 +26,15 @@ using namespace RavlImageN;
 
 int main(int nargs,char **argv) {
   OptionC opt(nargs,argv);
-  StringC fout = opt.String("o","@X","Output image ");
-  bool abs = opt.Boolean("a",false,"Calculate the absolute difference. ");
+  opt.Comment(" ");
+  opt.Comment("Computes the difference between two images, using grey level internal representation");
+  opt.Comment("Options provide basic grey level manipulation of the output.");
+  opt.Comment("Order of applying options: a off g c\n");
+  StringC fout = opt.String("o","@X","Output image. (NB \"@X\" display optimises gain & offset on display only.)");
+  bool abs = opt.Boolean("a","Calculate the absolute difference. ");
+  bool clip = opt.Boolean("c","Clip the result to lie between 0 and 255.");
+  IntT offset = opt.Int("off", 0, "Add a constant offset to the image.");
+  RealT gain = opt.Real("g", 1.0, "Adjust the gain of the image.");
   StringC fin1 = opt.String("","in1.pgm","Input image 1 ");
   StringC fin2 = opt.String("","in1.pgm","Input image 2 ");
   opt.Check();
@@ -49,10 +57,21 @@ int main(int nargs,char **argv) {
     res = in1 - in2;
   else {
     res = ImageC<IntT>(in1.Rectangle()); // Allocate some space.
-    for(Array2dIter3C<IntT,IntT,IntT> it(res,in1,in2);it;it++)
+    for (Array2dIter3C<IntT,IntT,IntT> it(res,in1,in2);it;it++)
       it.Data1() = Abs(it.Data2() - it.Data3());
   }
-  
+
+  if (gain != 1.0) res *= gain;
+
+  if (offset != 0) res += offset;
+
+  if (clip) {
+    for (Array2dIterC<IntT> it(res);it;it++) {
+      if (*it > 255) *it = 255;
+      else if (*it < 0) *it = 0;
+    }
+  }
+
   if(!Save(fout,res)) {
     std::cerr << "Failed to save output image '" << fout << "'\n";
     return 1;
