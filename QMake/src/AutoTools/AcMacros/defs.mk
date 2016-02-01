6f704f18a67386ea756eef30c39015b7e5932920
John Field <John.Field@surrey.ac.uk>
2016-02-01 12:18:17 +0000

    Macroise check for availability of package prereqs    No need to bump revision number to accommodate new macro as previous    version bump has not been pushed (so revision holds for both new macros)

--- QMake/src/AutoTools/AcMacros/defs.mk
+++ QMake/src/AutoTools/AcMacros/defs.mk
@@ -1,5 +1,5 @@
 # This file is part of QMake, Quick Make System
-# Copyright (C) 2012, University of Surrey
+# Copyright (C) 2012-16, University of Surrey
 # This code may be redistributed under the terms of the GNU General
 # Public License (GPL). See the gpl.licence file for details or
 # see http://www.gnu.org/copyleft/gpl.html
@@ -11,5 +11,6 @@ LOCAL_FILES=QM_CHOOSE_GETCWD QM_COMPILE_CK_FOR_LIB QM_DEFINE_EXTLIB_CC \
             QM_PROCESS_EXTLIBS QM_SET_ARCHIVER_ARFLAGS QM_SET_COMPILER_CFLAGS \
             QM_SET_MAKE_MAKEFLAGS QM_CK_COMPILE_CK_LIB QM_DEFINE_ALL_EXTLIBS \
             QM_CHECK_CC_LIB QM_LOCATE_QMAKE_DEPS QM_SET_SHAREDEXT \
-            QM_READ_USER_ENV QM_FINISH_DEFINE QM_RESOURCE_BASIC_SWITCHES
+            QM_READ_USER_ENV QM_FINISH_DEFINE QM_RESOURCE_BASIC_SWITCHES \
+            QM_CK_PREREQ_STATE
 
