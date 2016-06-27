f230d8f94bbe6528dd160c236ec8e0102163f90e
John Field <John.Field@surrey.ac.uk>
2016-06-27 17:40:28 +0100

    Allow explicit use of C and C++ standards

--- src/OS/Threads/Tools/defs.mk
+++ src/OS/Threads/Tools/defs.mk
@@ -1,5 +1,5 @@
 # This file is part of RAVL, Recognition And Vision Library 
-# Copyright (C) 2001, University of Surrey
+# Copyright (C) 2001-16, University of Surrey
 # This code may be redistributed under the terms of the GNU Lesser
 # General Public License (LGPL). See the lgpl.licence file for details or
 # see http://www.gnu.org/copyleft/lesser.html
@@ -8,9 +8,9 @@
 #! rcsid="$Id$"
 #! file="Ravl/OS/Threads/Tools/defs.mk"
 
-DESCRIPTION=Threading tools
+CXX_STD=CXX11
 
-ANSIFLAG=-std=c++11
+DESCRIPTION=Threading tools
 
 PACKAGE=Ravl/Threads
 
